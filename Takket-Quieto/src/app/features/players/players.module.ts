41dbb79481b52deff5f5daf4a814093306f0df52
Piotrek Budzowski <piobudzows@gmail.com>
2026-01-26 17:53:36 +0100

    limpieza

--- Takket-Quieto/src/app/features/players/players.module.ts
+++ Takket-Quieto/src/app/features/players/players.module.ts
@@ -5,14 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { PlayersRoutingModule } from './players-routing.module';
 import { PlayersPageComponent } from './page-player/players-page.component';
 import { PlayerListComponent } from './components/player-list/player-list.component';
-import { PlayerCardComponent } from './components/player-card/player-card.component';
+
 import { PlayerFormComponent } from './components/player-form/player-form.component';
 
 @NgModule({
     declarations: [
         PlayersPageComponent,
         PlayerListComponent,
-        PlayerCardComponent,
+
         PlayerFormComponent
     ],
     imports: [
